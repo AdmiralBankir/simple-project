fc7d65f964bcf44db2e5778c3446261d58814048
juliak <julia.khairova@fuse8.online>
2022-07-23 18:17:55 +0500

    подправила блок "Подберем всё"

--- src/main.js
+++ src/main.js
@@ -10,3 +10,12 @@ import '@/js/test-script.js';
 // images
 import '@/assets/whois.png';
 import '@/assets/header.png';
+import '@/assets/toroid.png';
+import '@/assets/toroid2.png';
+import '@/assets/item1.png';
+import '@/assets/item2.png';
+import '@/assets/item3.png';
+import '@/assets/item4.png';
+import '@/assets/item5.png';
+import '@/assets/item6.png';
+import '@/assets/item7.png';
