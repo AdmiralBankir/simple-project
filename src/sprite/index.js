bfff444899898ea6d7fd59240d032944bb1a7a6b
juliak <julia.khairova@fuse8.online>
2022-07-25 18:16:03 +0500

    иконки

--- src/sprite/index.js
+++ src/sprite/index.js
@@ -1,2 +1,8 @@
 import './arrow.svg';
 import './basket.svg';
+import './logoSmall.svg';
+import './logo.svg';
+import './search.svg';
+import './icon.svg';
+import './icon1.svg';
+import './icon2.svg';
