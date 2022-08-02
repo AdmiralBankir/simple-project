e08d8bb1d5e470adbfb5bda512972b287a6e7433
juliak <julia.khairova@fuse8.online>
2022-08-02 13:48:15 +0500

    формы битрикс

--- src/js/callback-form.js
+++ src/js/callback-form.js
@@ -1,23 +1,36 @@
-const URL = 'https://emex.bitrix24.ru/rest/9873/z56lfbjkxa3g1o1o/profile.json';
+//const URL = 'https://emex.bitrix24.ru/rest/9873/z56lfbjkxa3g1o1o/crm.lead.add.json';
+//const URL = 'https://emex.bitrix24.ru/rest/9873/xsr1kjknbu4mb5cg/crm.lead.add.json';
+const URL = 'https://emex.bitrix24.ru/rest/9873/h5s7egjkgzngmfr9/crm.lead.add.json';
 
-const forms = document.querySelectorAll('#callback-form');
+const forms = document.querySelectorAll('.form-bitrix24');
 
 function getFormData(form) {
-  const data = {};
+  const data = { fields: {} };
   const phoneNumberInput = form.querySelector('input[name="phone"]');
   const nameInput = form.querySelector('input[name="name"]');
   const vinInput = form.querySelector('input[name="vin"]');
 
+  if (form.classList.contains('.callback-form') || form.classList.contains('.callback-form-popup')) {
+    data.fields.TITLE = 'Обратный звонок kazan.emex.ru (test)';
+  } else if (form.classList.contains('.form-podbor') || form.classList.contains('.form-podbor-2')) {
+    data.fields.TITLE = 'Подбор по VIN kazan.emex.ru (test1)';
+  }
+
   if (phoneNumberInput) {
-    data.phoneNumber = phoneNumberInput.value;
+    data.fields.PHONE = {
+      'n0': {
+        'VALUE': phoneNumberInput.value,
+        'VALUETYPE': 'WORK',
+      },
+    };
   }
 
   if (nameInput) {
-    data.name = nameInput.value;
+    data.fields.NAME = nameInput.value;
   }
 
   if (vinInput) {
-    data.vin = vinInput.value;
+    data.fields.VIN = vinInput.value;
   }
 
   return data;
