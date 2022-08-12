e51aca970854206622929e5826a5513f4cb0e274
Vladislav Bodin <vladislav.bodin@fuse8.online>
2022-08-12 10:59:29 +0200

    adds new hook

--- src/js/callback-form.js
+++ src/js/callback-form.js
@@ -1,7 +1,9 @@
 //const URL = 'https://emex.bitrix24.ru/rest/9873/z56lfbjkxa3g1o1o/crm.lead.add.json';
 //const URL = 'https://emex.bitrix24.ru/rest/9873/xsr1kjknbu4mb5cg/crm.lead.add.json';
 //const URL = 'https://emex.bitrix24.ru/rest/9873/h5s7egjkgzngmfr9/crm.lead.add.json';
-const URL = 'https://emex.bitrix24.ru/rest/9873/o74mq9dvno1rroy7/crm.lead.add.json';
+// const URL = 'https://emex.bitrix24.ru/rest/9873/o74mq9dvno1rroy7/crm.lead.add.json';
+const URL = 'https://emex.bitrix24.ru/rest/9873/lj7wtefx2dn1u66g/crm.lead.add.json';
+
 import getCookie from './helpers/cookie';
 
 const forms = document.querySelectorAll('.form-bitrix24');
